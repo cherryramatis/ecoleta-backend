0dd0baa3234f6bf4b0476bcf15b7360f919145ae
FahRamatis <fah.ramatis@gmail.com>
2020-06-03 20:30:17 -0300

    a whole set of features

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import routes from "./routes";
+import path from "path";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  console.log("listagem de usuarios");
+app.use(express.json());
+app.use(routes);
 
-  res.json(["Daniel", "Cherry"]);
-});
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
